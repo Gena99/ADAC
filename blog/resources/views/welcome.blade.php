a5737630403955f3b1c5b84931d8792e7bff3a04
Aymeric <aymeric.gioia@hotmail.fr>
2017-07-28 14:47:36 +0200

    modif menu, responsive

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -37,31 +37,40 @@
         <li><a href="#">Gallerie</a></li>
         <li><a href="#">Contact</a></li>
         <li><a href="#">Presse</a></li>
-        <form class="navbar-form navbar-left">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <!-- Authentication Links -->
+        @if (Auth::guest())
+        <li><a href="{{ route('login') }}">Se connecter</a></li>
+        <li><a href="{{ route('register') }}">S'inscrire</a></li>
+        @else
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+
+            <ul class="dropdown-menu" role="menu">
+                <li>
+                    <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    Se deconnecter
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </li>
         </ul>
     </li>
+    @endif
 </ul>
-</div><!-- /.navbar-collapse -->
-</div><!-- /.container-fluid -->
+</div>  
+
+</div>
+
 </nav>
-<!--<ul class="nav navbar-nav navbar-right">
-    <li><a href="{{ route('login') }}">Se connecter</a></li>
-    <li><a href="{{ route('register') }}">S'enregistrer</a></li>
-</ul>-->
+<script src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
